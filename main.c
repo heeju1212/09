f4039a082c2462422125f636088875d5638e70ce
heeju1212 <heeju4746@gmail.com>
2023-11-03 18:08:40 +0900

    실습 2번-2번째

--- main.c
+++ main.c
@@ -6,17 +6,19 @@
 
 int main(int argc, char *argv[]) {
 	int grade[N_STUDENT];
-	int i;
+	int i,average;
+	int sum;
 	printf("input 5 scores :");
 	
+	sum = 0;
 	for (i=0;i<N_STUDENT;i++){
 		scanf("%d",&grade[i]);
+		sum += grade[i];
 	}
 	
+	average = sum / N_STUDENT;
 	
-	for (i=0;i<N_STUDENT;i++){
-		printf("grade[%d] = %d\n",i,grade[i]);
-	}
+	printf("score average : %d\n",average);
 	
 	system("PAUSE");
 	return 0;
